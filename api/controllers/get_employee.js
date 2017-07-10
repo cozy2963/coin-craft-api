52700fd845d6dda4d4961b1983814a647a68f45b
Aisha Blake <aisha.g.blake@gmail.com>
2017-07-10 13:41:57 -0400

    GET employee expenses

--- api/controllers/get_employee.js
+++ api/controllers/get_employee.js
@@ -10,10 +10,12 @@ admin.initializeApp({
 });
 
 var db = admin.database();
-var ref = db.ref("employees");
+var employeesRef = db.ref("employees");
+var expensesRef = db.ref("expenses");
 
 module.exports = {
-  getEmployee: getEmployee
+  getEmployee: getEmployee,
+  getExpenses: getExpenses
 };
 
 function getEmployee(req, res) {
@@ -21,10 +23,34 @@ function getEmployee(req, res) {
   var id = req.swagger.params.auth_id.value;
   var employee;
 
-  ref.orderByChild("auth_id").equalTo(id).once("value", function(snapshot) {
+  employeesRef.orderByChild("auth_id").equalTo(id).once("value", function(snapshot) {
     var name = Object.getOwnPropertyNames(snapshot.val());
     employee = snapshot.val()[name];
     res.json(employee);
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+}
+
+function getExpenses(req, res) {
+  // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
+  var id = req.swagger.params.auth_id.value;
+
+  expensesRef.orderByChild("employee_auth_id").equalTo(id).once("value", function(snapshot) {
+    var expenses = Object.getOwnPropertyNames(snapshot.val());
+    var resExpenses = [];
+    expenses.forEach(function(expense) {
+      resExpenses.push({
+        amount: snapshot.val()[expense].amount,
+        business_name: snapshot.val()[expense].business_name,
+        client: snapshot.val()[expense].client,
+        expense_type: snapshot.val()[expense].expense_type,
+        miles_amount: snapshot.val()[expense].miles_amount,
+        receipt: snapshot.val()[expense].receipt,
+        submitted: snapshot.val()[expense].submitted
+      });
+    });
+    res.json(resExpenses);
   }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
   });
