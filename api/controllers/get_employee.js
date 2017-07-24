a0731c8e0e592adc655ef0a676658f62200a1d03
Aisha Blake <aisha.g.blake@gmail.com>
2017-07-24 18:10:00 -0400

    Access request body to get new expense to be submitted

--- api/controllers/get_employee.js
+++ api/controllers/get_employee.js
@@ -64,45 +64,51 @@ function getExpenses(req, res) {
 
 function createExpense(req, res) {
 
-  var expense = req.swagger.params.expense.value;
-  var amount;
-  var employee;
+  var expense = req.body;
+  var amount, employee, balance;
 
   employeesRef.orderByChild("auth_id").equalTo(expense.employee_auth_id).once("value", function(snapshot) {
-    var name = Object.getOwnPropertyNames(snapshot.val());
+    var name = Object.getOwnPropertyNames(snapshot.val())[0];
     employee = snapshot.val()[name];
+
+    if(expense.receipt_type == 'mileage') {
+      amount = expense.miles_amount * 0.575;
+    } else {
+      amount = expense.expense_amount;
+    }
+
+    balance = Number(employee.current_balance) - Number(amount);
+
+    var roundedAmount = Number((Math.round(amount * 100) / 100).toFixed(2));
+    if (balance >= 0) {
+      expensesRef.push({
+        employee_name: employee.name,
+        employee_auth_id: expense.employee_auth_id,
+        submitted_date: new Date().toLocaleDateString(),
+        expense_type: expense.expense_type,
+        expense_amount: roundedAmount,
+        receipt_type: expense.receipt_type,
+        receipt_date: expense.receipt_date,
+        approved_by: expense.approved_by,
+        expense_business_name: expense.expense_business_name,
+        miles_amount: Number((Math.round(expense.miles_amount * 100) / 100).toFixed(2)) || 0,
+        client_name: expense.client_name,
+        expense_description: expense.expense_description
+      }).then(_ => {
+        if (expense.expense_type !== "notCoinCraft") {
+          var employeeRef = employeesRef.child(name);
+          employeeRef.update({"current_balance": balance}).then(_ => {
+            console.log("It updated!");
+          }, error => {
+            console.log(error);
+          });
+        }
+        console.log("Submitted expense worked!");
+      }, error => {
+        console.log(error);
+      });
+    }
   }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
   });
-
-  if(expense.receipt_type == 'mileage') {
-    amount = expense.miles_amount * 0.575;
-  } else {
-    amount = expense.expense_amount;
-  }
-
-  var balance = Number(employee.current_balance) - Number(amount);
-
-  if(employee.current_balance >= 0) {
-    var roundedAmount = (Math.round(amount * 100) / 100).toFixed(2);
-
-    expensesRef.push({
-      employee_name: employee.name,
-      employee_auth_id: expense.employee_auth_id,
-      submitted_date: new Date().toLocaleDateString(),
-      expense_type: expense.expense_type,
-      expense_amount: roundedAmount,
-      receipt_type: expense.receipt_type,
-      receipt_date: expense.receipt_date,
-      approved_by: expense.approved_by,
-      expense_business_name: expense.expense_business_name,
-      miles_amount: Number((Math.round(expense.miles_amount * 100) / 100).toFixed(2)),
-      client_name: expense.client_name,
-      expense_description: expense.expense_description
-    }).then(_ => {
-      if (expense.expense_type !== "notCoinCraft") {
-        employeesRef.update(employee.auth_id, {current_balance: balance});
-      });
-    }
-  }
 }
